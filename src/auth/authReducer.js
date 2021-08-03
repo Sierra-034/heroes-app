73d82ae24598a0e377df3cc02ef7bb1f86af88bc
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2021-08-03 10:41:03 -0500

    188: Context y Reducer de mi aplicación

---
+++ src/auth/authReducer.js
@@ -0,0 +1,24 @@
+import { types } from "../types/types";
+
+// const state = {
+//     name: 'Samuel',
+//     logged: true
+// };
+
+export const authReducer = (state = {}, action) => {
+    switch (action.type) {
+        case types.login:
+            return {
+                ...action.payload,
+                logged: true
+            };
+            break;
+
+        case types.logout:
+            return { logged: false }
+            break;
+
+        default:
+            return state;
+    }
+};
